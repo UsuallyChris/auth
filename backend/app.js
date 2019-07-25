ab01b457f3183227f23a83d36bce003e9d428fee
UsuallyChris <schoonc@gmail.com>
2019-07-24 18:02:32 -0700

    Basic login and logout routes roughed in.

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,10 @@ const app = express();
 
 const PORT = 5000;
 
-app.get('/', (req, res) => {
-  res.send(`Server running on port ${PORT}.`)
-});
+// Route Imports
+const auth = require('./routes/auth-routes');
+
+// Route Configs
+app.use('/api/auth/', auth);
 
 app.listen(PORT);
--- backend/routes/auth-routes.js
+++ backend/routes/auth-routes.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/login', (req, res) => {
+  res.send('Login route');
+})
+
+router.get('/logout', (req, res) => {
+  res.send('Logout route');
+})
+
+module.exports = router;
